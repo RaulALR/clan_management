2a2ba459e188ed63c92199823becc0a8e8c21418
RaulALR <raulalbarranlr@gmail.com>
2025-03-03 14:44:30 +0100

    Fix docker

--- api/src/app.js
+++ api/src/app.js
@@ -10,13 +10,34 @@ const matchRoutes = require("./routes/match.routes");
 
 const app = express();
 
-app.use(express.json({ limit: "50mb" })); 
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 // app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:4200", credentials: true }));
+app.use(
+  cors({
+    origin: [],
+    credentials: true,
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+  })
+);
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin"); // O "*"
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+  
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(204);
+    }
+  
+    next();
+  });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/players", playerRoutes);
