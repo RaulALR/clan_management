8976e4dc074bb063c571116a8640942d5fd73f1b
RaulALR <raulalbarranlr@gmail.com>
2025-03-10 20:55:44 +0100

    fix async

--- bots/botsController.py
+++ bots/botsController.py
@@ -5,34 +5,34 @@ import getMembers.main
 from config import SERVER_URL, BOT_GET_MEMBERS
 
 logger = logging.getLogger(__name__)
-sio = socketio.Client()
+sio = socketio.AsyncClient()
 
-def start_io():
+async def start_io():
     @sio.event
-    def connect():
+    async def connect():
         print("✅ Conectado al servidor WebSocket")
-        sio.emit("registerBot", BOT_GET_MEMBERS)
+        await sio.emit("registerBot", BOT_GET_MEMBERS)
 
     @sio.on("executeCommand")
-    def execute_command(data):
+    async def execute_command(data):
         bot_name = data.get("botName")
         command = data.get("command")
-        if bot_name == BOT_GET_MEMBERS:
-            bot_loop = getMembers.main.bot.loop
-            future = asyncio.run_coroutine_threadsafe(
-                getMembers.main.execute_command(command), bot_loop
-            )
-            response = future.result()
 
-            sio.emit("commandResponse", {"bot": bot_name, "response": response})    
+        if bot_name == BOT_GET_MEMBERS:
+
+            task = getMembers.main.bot.loop.create_task(getMembers.main.execute_command(command))
+            response = await task
+
+            await sio.emit("commandResponse", {"bot": bot_name, "response": response})
+
 
     @sio.event
-    def disconnect():
+    async def disconnect():
         print("❌ Desconectado del servidor WebSocket")
 
     try:
-        sio.connect(SERVER_URL)
-        sio.wait()
+        await sio.connect(SERVER_URL)
+        await sio.wait()
     except Exception as e:
         logging.error(f"Error al conectar WebSocket: {e}")
 
--- bots/main.py
+++ bots/main.py
@@ -9,13 +9,10 @@ dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 def run_bots_controller():
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    loop.run_until_complete(botsController.start_io())
+    asyncio.run(botsController.start_io())
 
 if __name__ == "__main__":
     ws_thread = threading.Thread(target=run_bots_controller, daemon=True)
-    print(ws_thread)
     ws_thread.start()
 
     getMembers.main.run_bot()
